0910758770a73e5b824f117ba18809e4f8ac0027
Sebastian Sierra <juansebastiansierrac@gmail.com>
2020-07-15 00:03:44 -0500

    Validacion del color selecionado y otros fixes

--- js/main.js
+++ js/main.js
@@ -3,19 +3,25 @@ const btnRed = document.getElementById('red');
 const btnYellow = document.getElementById('yellow');
 const btnBlue = document.getElementById('blue');
 const btnStart = document.getElementById('btnStart');
+const lblTurnInfo = document.getElementById('turnInfo');
 
 const COLORS_INTERVAL = 1000;
 const LIGHT_INTERVAL = 500;
+const LAST_LEVEL = 10;
 class Game {
     constructor() {
         this.init();
         this.genSequence();
-        this.nextLevel();
+        setTimeout(() => {
+            this.nextLevel();
+        }, 1000)
     }
 
     init() {
+        this.chooseColor = this.chooseColor.bind(this);
+        this.nextLevel = this.nextLevel.bind(this);
         btnStart.classList.add('hide');
-        this.level = 10;
+        this.level = 1;
         this.colors = {
             green: btnGreen,
             red: btnRed,
@@ -25,19 +31,22 @@ class Game {
     }
 
     genSequence() {
-        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+        this.sequence = new Array(LAST_LEVEL).fill(0).map(n => Math.floor(Math.random() * 4));
     }
 
-    async nextLevel() {
-        this.lightSequence();
-        this.clickEvents();
+    nextLevel() {
+        this.subLevel = 0;
+        lblTurnInfo.innerHTML = 'Watch'
+        this.lightSequence().then(() => this.clickEvents());
     }
 
-    lightSequence() {
+    async lightSequence() {
         for (let i = 0; i < this.level; i++) {
             const color = this.numToColor(this.sequence[i]);
             setTimeout(() => this.lightColor(color), COLORS_INTERVAL * i);
         }
+        console.log((COLORS_INTERVAL + LIGHT_INTERVAL)* this.level);
+        return await  new Promise ((resolve, reject)=> setTimeout(resolve, (COLORS_INTERVAL)* this.level))
     }
 
     lightColor(color){
@@ -50,19 +59,50 @@ class Game {
     }
 
     clickEvents(){
-        this.colors.green.classList.add('usr');
-        this.colors.red.classList.add('usr');
-        this.colors.yellow.classList.add('usr');
-        this.colors.blue.classList.add('usr');
-
         this.colors.green.addEventListener('click', this.chooseColor);
         this.colors.red.addEventListener('click', this.chooseColor);
         this.colors.yellow.addEventListener('click', this.chooseColor);
         this.colors.blue.addEventListener('click', this.chooseColor);
+
+        this.colors.green.classList.add('usr');
+        this.colors.red.classList.add('usr');
+        this.colors.yellow.classList.add('usr');
+        this.colors.blue.classList.add('usr');
+        lblTurnInfo.innerHTML = 'Play'
+    }
+
+    removeClickEvents(){
+        this.colors.green.removeEventListener('click', this.chooseColor);
+        this.colors.red.removeEventListener('click', this.chooseColor);
+        this.colors.yellow.removeEventListener('click', this.chooseColor);
+        this.colors.blue.removeEventListener('click', this.chooseColor);
+
+        this.colors.green.classList.remove('usr');
+        this.colors.red.classList.remove('usr');
+        this.colors.yellow.classList.remove('usr');
+        this.colors.blue.classList.remove('usr');
     }
 
     chooseColor(ev){
-        console.log(ev);
+        const color = ev.target.id;
+        const num = this.colorToNum(color);
+        this.lightColor(color);
+        if(num === this.sequence[this.subLevel]){
+            this.subLevel++;
+            if(this.subLevel === this.level){
+                this.level++;
+                this.removeClickEvents();
+
+                if(this.level === (LAST_LEVEL +1)){
+                    console.log('ganaste');
+                }else{
+                    setTimeout(this.nextLevel, 1500);
+                }
+            }
+
+        }else{
+            console.log('la cagaste');
+        }
     }
 
     numToColor(num) {
@@ -78,6 +118,25 @@ class Game {
                 break;
             case 3:
                 return 'blue'
+                break;
+            default:
+                break;
+        }
+    }
+
+    colorToNum(color) {
+        switch (color) {
+            case 'green':
+                return 0
+                break;
+            case 'red':
+                return 1
+                break;
+            case 'yellow':
+                return 2
+                break;
+            case 'blue':
+                return 3
                 break;
             default:
                 break;
